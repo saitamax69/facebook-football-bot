e427d8094c08700fc637eed3c307d3fa7e78a5d8
khassidi iliasse <114320005+saitamax69@users.noreply.github.com>
2026-01-17 19:36:26 +0100

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,30 +1,4 @@
 """
-Utility functions
+Utils
 """
-from datetime import datetime
-import pytz
-
-
-def format_date(dt):
-    """Format: Monday, 15 January 2025"""
-    return dt.strftime('%A, %d %B %Y')
-
-
-def format_time(dt):
-    """Format: HH:MM"""
-    return dt.strftime('%H:%M')
-
-
-def odds_to_probability(odds):
-    """Convert decimal odds to probability"""
-    return round((1 / odds) * 100, 1) if odds > 0 else 0
-
-
-def calculate_profit(odds, stake, won):
-    """Calculate profit/loss"""
-    return round(stake * odds - stake, 2) if won else -stake
-
-
-def get_current_utc():
-    """Get current UTC time"""
-    return datetime.now(pytz.UTC)
+def format_date(d): return d.strftime('%Y-%m-%d')
